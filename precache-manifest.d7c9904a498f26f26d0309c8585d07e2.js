ba0ea581075ce5cbf0bcba8174f38a89537285f3
Shuyan He <shuyanh@gmail.com>
2020-05-20 18:06:44 -0400

    Updates

---
+++ precache-manifest.d7c9904a498f26f26d0309c8585d07e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6233fd2f3c7fe854dc6f730136f815",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "99d8fb1bddb9fe61a275",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "99d8fb1bddb9fe61a275",
+    "url": "/contactmanager/static/js/2.caffc9ff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.caffc9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1621dce639f8e7f38da4",
+    "url": "/contactmanager/static/js/main.b4ab9f84.chunk.js"
+  },
+  {
+    "revision": "f0f356433407649f9daf",
+    "url": "/contactmanager/static/js/runtime-main.f6c193b8.js"
+  }
+]);
